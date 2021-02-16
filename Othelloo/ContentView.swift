e0225ec2e09e08ea1cc46b71b07905a17bd4c7be
juandahurt <juandahurtadom@gmail.com>
2021-02-16 10:12:08 -0500

    Initial Commit

---
+++ Othelloo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Othelloo
+//
+//  Created by juandahurt on 16/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Othelloo/OthellooApp.swift
@@ -0,0 +1,17 @@
+//
+//  OthellooApp.swift
+//  Othelloo
+//
+//  Created by juandahurt on 16/02/21.
+//
+
+import SwiftUI
+
+@main
+struct OthellooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
